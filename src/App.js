16873fa843128778501ec90e007eb239a9a4f7ab
iamLolade <adeniyiotemade@gmail.com>
2022-04-15 02:10:31 +0100

    Created array of images & Shuffle function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./styles/App/App.css"
 
+const cardImages = [
+  {"src": "/images/1.png"},
+  {"src": "/images/2.png"},
+  {"src": "/images/5.png"},
+  {"src": "/images/6.png"},
+  {"src": "/images/9.png"},
+  {"src": "/images/10.png"},
+  {"src": "/images/11.png"},
+  {"src": "/images/12.png"},
+  {"src": "/images/4.png"},
+  {"src": "/images/8.png"}
+];
+
+const shuffleCards = () => {
+
+};
+
 function App() {
   return (
     <div className="App">
